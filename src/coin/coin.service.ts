8fd6714c096ce2846e95c1734abbd74516fda8c6
Vladyslav0060 <vlad.bortnik1@gmail.com>
2022-07-27 22:27:45 +0300

    valadation

--- src/coin/coin.service.ts
+++ src/coin/coin.service.ts
@@ -1,49 +1,70 @@
 import { HttpException, Injectable } from '@nestjs/common';
 import axios, { AxiosResponse } from 'axios';
-import { ValidateIf } from 'class-validator';
-import { object, string, date, array, number, mixed } from 'yup';
+import { object, string, date, array, number } from 'yup';
+import Ajv from 'ajv';
+const ajv = new Ajv();
 
-export const res = object({
-  data: array()
-    .required()
-    .of(
-      object().shape({
-        id: string().required(),
-        symbol: string().required(),
-        name: string().required(),
-        image: string().required(),
-        current_price: number().required(),
-        market_cap: number().required(),
-        market_cap_rank: number().required(),
-        total_volume: number().required(),
-        high_24h: number().required(),
-        low_24h: number().required(),
-        price_change_24h: number().required(),
-        price_change_percentage_24h: number().required(),
-        market_cap_change_24h: number().required(),
-        market_cap_change_percentage_24h: number().required(),
-        circulating_supply: number().required(),
-        total_supply: number().required(),
-        ath: number().required(),
-        ath_change_percentage: number().required(),
-        ath_date: date().required(),
-        atl: number().required(),
-        atl_change_percentage: number().required(),
-        atl_date: date().required(),
-        last_updated: date().required(),
-      }),
-    ),
-});
+const testSchema = {
+  type: 'object',
+  properties: {
+    id: { type: 'string' },
+  },
+};
+
+const schema = {
+  type: 'object',
+  properties: {
+    data: {
+      type: 'array',
+      properties: {
+        id: { type: 'string' },
+        symbol: { type: 'string' },
+        name: { type: 'string' },
+        image: { type: 'string' },
+        current_price: { type: 'number' },
+        market_cap: { type: 'number' },
+        market_cap_rank: { type: 'number' },
+        total_volume: { type: 'number' },
+        high_24h: { type: 'number' },
+        low_24h: { type: 'number' },
+        price_change_24h: { type: 'number' },
+        price_change_percentage_24h: { type: 'number' },
+        market_cap_change_24h: { type: 'number' },
+        market_cap_change_percentage_24h: { type: 'number' },
+        circulating_supply: { type: 'number' },
+        total_supply: { type: 'number' },
+        ath: { type: 'number' },
+        ath_change_percentage: { type: 'number' },
+        ath_date: { type: 'number' },
+        atl: { type: 'number' },
+        atl_change_percentage: { type: 'number' },
+        atl_date: { type: 'string' },
+        last_updated: { type: 'string' },
+      },
+    },
+  },
+  required: ['data'],
+};
 
 @Injectable()
 export class CoinService {
   async getAssets(body: any) {
     const url = process.env.URL_GET_ASSETS;
     const response: AxiosResponse<any> = await axios.get(url);
-    console.log(response);
-    await res.validate(response).catch((err) => {
-      throw new HttpException(err.message, 400);
-    });
+
+    const testObj = {
+      id: 1,
+    };
+    const testValidate = ajv.compile(testSchema);
+    const testValid = testValidate(testObj);
+    console.log({ testValid });
+
+    const validate = ajv.compile(schema);
+    const valid = validate(response);
+    console.log({ valid });
+    // await res.validate(response).catch((err) => {
+    //   throw new HttpException(err.message, 400);
+    // });
     return response.data;
   }
 }
